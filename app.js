f9a6cb13cee9f5386e5abc6120f0d2a57d22586b
Yan Hong <onlyhongyan@gmail.com>
2017-03-18 16:03:33 -0700

    post new student to db

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
 
 app.set('view engine', 'pug')
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
 mongoose.connect('mongodb://localhost/webdxd')
 
@@ -32,7 +35,13 @@ app.get('/', function (req, res) {
 })
 
 app.get('/new', function (req,res) {
-    res.send('Create New')
+    res.render(path.join(__dirname, './views', 'add.pug'), {})
+})
+
+app.post('/new', function(req, res) {
+    var studentObject = new Student(req.body)
+    studentObject.save()
+    res.send("Success")
 })
 
 app.get('/:id', function (req, res) {
